265217d969bb83a25806805e3c90b489275cf2f4
Enriquebc13 <baezenrique114@gmail.com>
2026-06-03 16:02:54 -0600

    Implementación de endpoint GET /cats

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,12 @@
-import {Controller } from '@nestjs/common';
+import {Controller, Get } from '@nestjs/common';
+import { Cat } from './entities/cat.entity';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+    constructor(private catsService: CatsService) { }
+    @Get()
+    findAll(): Cat[] {
+        return this.catsService.findAll();
+    }
+}
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Cat } from './entities/cat.entity';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+    private cats: Cat[] = [
+        {
+            id: 1,
+            name: 'Michi',
+            age: 2,
+            breed: 'Siamés',
+        },
+        {
+            id: 2,
+            name: 'Manchas',
+            age: 1,
+            breed: 'Persa',
+        },
+        {
+            id: 3,
+            name: 'Pelusa',
+            age: 3,
+            breed: 'Angora',
+        },
+    ];
+
+    findAll(): Cat[] {
+        return this.cats;
+    }
+}
