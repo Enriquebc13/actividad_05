3a1676c30de8784d7a29b36add72e1a7c1d63596
Enriquebc13 <baezenrique114@gmail.com>
2026-06-05 14:51:16 -0600

    Creación de la estructura del módulo Players

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, PlayersModule],
   controllers: [AppController],
   providers: [AppService],
 })
